3665ae0bcee3a73ec947cf6b6c9e816249fafdf4
Muhammad Arif <arif1951995@gmail.com>
2019-07-08 00:10:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let input = document.querySelector('input');
+fetch(`https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json`)
+.then(result => {
+    return result.json();
+})
+.then(data => {
+    document.querySelector('input').addEventListener('keyup', () => {
+        document.querySelector('ul').innerHTML = '';
+        if(document.querySelector('input').value === '') {
+        document.querySelector('ul').innerHTML = '';
+
+        }
+    data.forEach(e => {
+        if(e.city.toLowerCase().includes(document.querySelector('input').value) && document.querySelector('input').value !== '') {
+            document.querySelector('ul').insertAdjacentHTML('afterbegin', `<li> ${e.city.slice(0, e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)))}    <span>${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)),  e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1)}</span> ${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1 )} </li>`)
+        }
+    });
+    
+    })
+})
+.catch(error => {
+    document.write(error);
+}) 
