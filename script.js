14fbdeab8882b1a638618334c7330379be8e7515
Muhammad Arif <arif1951995@gmail.com>
2019-07-08 02:54:49 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,24 +8,20 @@ fetch(`https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf2
     //    typeAhead(data);
     // })
     document.querySelector('input').addEventListener('input', () => {
-        typeAhead(data);
+        console.log(input.value)
+        document.querySelector('ul').innerHTML = '';
+        if(document.querySelector('input').value === '') {
+        document.querySelector('ul').innerHTML = '';
+    
+        }
+    data.forEach(e => {
+        if(e.city.toLowerCase().includes(document.querySelector('input').value) && document.querySelector('input').value !== '') {
+            document.querySelector('ul').insertAdjacentHTML('afterbegin', `<li> ${e.city.slice(0, e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)))}    <span>${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)),  e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1)}</span> ${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1 )} </li>`)
+        }
+    });
      })
 })
 .catch(error => {
     document.write(error);
 }) 
 
-function typeAhead(data) {
-    alert(input.value);
-    document.querySelector('ul').innerHTML = '';
-    if(document.querySelector('input').value === '') {
-    document.querySelector('ul').innerHTML = '';
-
-    }
-data.forEach(e => {
-    if(e.city.toLowerCase().includes(document.querySelector('input').value) && document.querySelector('input').value !== '') {
-        document.querySelector('ul').insertAdjacentHTML('afterbegin', `<li> ${e.city.slice(0, e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)))}    <span>${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(0)),  e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1)}</span> ${e.city.slice(e.city.toLowerCase().indexOf(input.value.toLowerCase().charAt(input.value.length -1)) + 1 )} </li>`)
-    }
-});
-
-}
